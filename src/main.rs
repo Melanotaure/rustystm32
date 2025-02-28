7e1ef6fa677cbedaf840cd3ea97fe606c6161b9d
unknown <luc.gaborit@gmail.com>
2025-02-28 17:35:12 +0100

    Bare metal way with write_volatile

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use core::ptr::write_volatile;
+
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
 use panic_halt as _;
@@ -12,27 +14,30 @@ fn main() -> ! {
     const RCC_AHB1ENR: *mut u32 = (RCC_BASE + 0x30) as *mut u32;
     const GPIOD_MODER: *mut u32 = (GPIOD_BASE + 0x00) as *mut u32;
     const GPIOD_ODR: *mut u32 = (GPIOD_BASE + 0x14) as *mut u32;
-    const PIN: u32 = 15;
+    const PIN: u32 = 15; // Blue LED
     unsafe {
         // Enable the clock for GPIOD
         *RCC_AHB1ENR |= 1 << 3;
         // Set pin 12 as output
         *GPIOD_MODER |= 1 << (PIN * 2);
     }
+
+    let mut blink = true;
     loop {
-        unsafe {
-            // Set pin 12 high
-            *GPIOD_ODR |= 1 << PIN;
+        if blink {
+            unsafe {
+                // Set pin 12 high
+                write_volatile(GPIOD_ODR, *GPIOD_ODR | (1 << PIN));
+            }
+        } else {
+            unsafe {
+                // Set pin 12 low
+                write_volatile(GPIOD_ODR, *GPIOD_ODR & !(1 << PIN));
+            }
         }
         for _ in 0..100_000 {
             nop();
         }
-        unsafe {
-            // Set pin 12 low
-            *GPIOD_ODR &= !(1 << PIN);
-        }
-        for _ in 0..100_000 {
-            nop();
-        }
+        blink = !blink;
     }
 }
