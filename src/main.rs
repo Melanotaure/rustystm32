88b9aede399d78d0a119ead47212b99cf1bd9ea4
unknown <luc.gaborit@gmail.com>
2025-02-28 22:09:59 +0100

    using stm32 hal2 for spinning leds

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,13 @@ use panic_halt as _;
 #[cfg(feature = "rtt")]
 use rtt_target::{rprintln, rtt_init_print};
 
+enum Led {
+    D15,
+    D14,
+    D13,
+    D12,
+}
+
 #[entry]
 fn main() -> ! {
     #[cfg(feature = "rtt")]
@@ -16,23 +23,53 @@ fn main() -> ! {
     let p = hal::pac::Peripherals::take().unwrap();
     p.RCC.ahb1enr.write(|w| w.gpioden().enabled());
     let mut pd15 = hal::gpio::Pin::new(hal::gpio::Port::D, 15, hal::gpio::PinMode::Output);
+    let mut pd14 = hal::gpio::Pin::new(hal::gpio::Port::D, 14, hal::gpio::PinMode::Output);
+    let mut pd13 = hal::gpio::Pin::new(hal::gpio::Port::D, 13, hal::gpio::PinMode::Output);
+    let mut pd12 = hal::gpio::Pin::new(hal::gpio::Port::D, 12, hal::gpio::PinMode::Output);
 
-    let mut blink = true;
+    let mut led_state = Led::D15;
     loop {
-        if blink {
-            // Set pin 15 high
-            pd15.set_high();
-            #[cfg(feature = "rtt")]
-            rprintln!("LED ON");
-        } else {
-            // Set pin 15 low
-            pd15.set_low();
-            #[cfg(feature = "rtt")]
-            rprintln!("LED OFF");
+        match led_state {
+            Led::D15 => {
+                #[cfg(feature = "rtt")]
+                rprintln!("D15");
+                pd15.set_high();
+                pd14.set_low();
+                pd13.set_low();
+                pd12.set_low();
+                led_state = Led::D14;
+            }
+            Led::D14 => {
+                #[cfg(feature = "rtt")]
+                rprintln!("D14");
+                pd15.set_low();
+                pd14.set_high();
+                pd13.set_low();
+                pd12.set_low();
+                led_state = Led::D13;
+            }
+            Led::D13 => {
+                #[cfg(feature = "rtt")]
+                rprintln!("D13");
+                pd15.set_low();
+                pd14.set_low();
+                pd13.set_high();
+                pd12.set_low();
+                led_state = Led::D12;
+            }
+            Led::D12 => {
+                #[cfg(feature = "rtt")]
+                rprintln!("D12");
+                pd15.set_low();
+                pd14.set_low();
+                pd13.set_low();
+                pd12.set_high();
+                led_state = Led::D15;
+            }
         }
+
         for _ in 0..100_000 {
             nop();
         }
-        blink = !blink;
     }
 }
